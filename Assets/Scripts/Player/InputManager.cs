b1c767ef21dd8255bd5c8c00128f80c55e67ada2
hungbq02 <hungbq.k2@gmail.com>
2024-03-04 20:24:20 +0700

    update model

---
+++ Assets/Scripts/Player/InputManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private PlayerControls playerControls;
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+    }
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+    public Vector2 GetPlayerMovement()
+    {
+        return playerControls.Player.Movement.ReadValue<Vector2>();
+    }        
+    public Vector2 GetMouseDelta()
+    {
+        return playerControls.Player.Look.ReadValue<Vector2>();
+    }    
+    public bool PlayerJumpedThisFrame()
+    {
+        return playerControls.Player.Jump.triggered;
+    }    
+}
---
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    Animator animator;
+    private Rigidbody rb;
+    private string currentState;
+
+    private float groundCheckDistance;
+
+    private float speedWalk = 5f;
+    private float jumpForce = 0;
+    public bool isGrounded;
+
+    private bool isShootPressed;
+    private bool isShooting;
+
+    [SerializeField] private float attackDelay = 0.3f;
+
+    //ANIMATION STATE
+    const string PLAYER_IDLE = "IdleBattle01_AR_Anim";
+    const string PLAYER_JUMP = "Jump_AR_Anim";
+    
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        // GROUND CHECK
+        groundCheckDistance = GetComponent<CapsuleCollider>().height / 2f;
+
+        RaycastHit hit;
+      //  Debug.DrawRay(transform.position, -transform.up * groundCheckDistance, Color.red);
+        if (Physics.Raycast(transform.position, -transform.up, out hit, groundCheckDistance))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    void ChangeAnimationState(string newState)
+    {
+        if (currentState == newState) return;
+
+        animator.Play(newState);
+        currentState = newState;
+    }    
+
+}
